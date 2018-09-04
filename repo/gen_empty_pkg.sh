f2eaa9c20979fe67a237ed32af88b0d48525758b
Jan-Daniel Kaplanski <kaplanski@uni-potsdam.de>
2018-09-04 23:29:50 +0200

    repo updated

--- repo/gen_empty_pkg.sh
+++ repo/gen_empty_pkg.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-arch=${1%/}
-if [ "$arch" == "i386" -o "$arch" == "amd64" -o "$arch" == "python2" -o "$arch" == "python3" -a "$2" != "" -a "$3" != "" ]; then
-   echo "Running for $arch:"
-   cd "$arch"
-   echo "  Creating $2_v$3 folder..."
-   mkdir "$2_v$3" && cd $2_v$3
-   echo "  Creating $2_display.txt..."
-   echo "standard display file for $2" > $2_display.txt
-   echo "  Creating $2_install.sh..."
-   touch $2_install.sh && chmod ugo+x $2_install.sh
-   echo "  Creating nofile..."
-   touch nofile
-   echo "  remove 'nofile' if binary/script is called $2[.bin/.sh/.py]"
-   echo "Done for $arch!"
+if [ "$1" != "" -a "$2" != "" -a "$3" != "" ]; then
+   arch=${1%/}
+   if [ "$arch" == "i386" -o "$arch" == "amd64" -o "$arch" == "python2" -o "$arch" == "python3" ]; then
+      echo "Running for $arch:"
+      cd "$arch"
+      echo "  Creating $2_v$3 folder..."
+      mkdir "$2_v$3"; cd $2_v$3
+      echo "  Creating $2_display.txt..."
+      echo "standard display file for $2" > $2_display.txt
+      echo "  Creating $2_install.sh..."
+      cp ../../samplefiles/sample_install.sh $2_install.sh
+      echo "  Creating nofile..."
+      echo "  (remove 'nofile' manually if binary/script is called $2[.bin/.sh/.py])"
+      touch nofile
+      echo "Done for $arch!"
+   else
+      echo "Unknown architecture!"
+   fi
 else
    echo "Usage: $0 [arch] [packagename] [version]"
 fi
---
+++ repo/samplefiles/sample_install.sh
@@ -0,0 +1,21 @@
+#automated install script
+#leave settings on default to work!
+#ver is either 'lite' or 'full'
+
+pkgfldr=$1
+infldr=$2
+pkgname=$3
+ver=$4
+
+if [ ! -d "$infldr/$pkgname" ]; then
+   echo "Initial creation of $pkgfldr/$pkgname!"
+   mkdir "$infldr/$pkgname"
+   echo "Done!"
+fi
+
+echo "Installing $pkgname..."
+cp * "$infldr/$pkgname"
+#if [ "$ver" == "lite" ]; then
+#   echo "alias [name]=$infldr/$pkgname/[file]" >> ~/.bashrc
+#fi
+echo "Done!"
