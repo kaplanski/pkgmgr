de82fc6a9bc1968eae03c8dc0083eb323182f088
kaplanski <kaplanski@uni-potsdam.de>
2018-09-04 20:18:49 +0200

    added empty package creation utility

--- repo/gen_empty_pkg.sh
+++ repo/gen_empty_pkg.sh
@@ -3,9 +3,13 @@ arch=${1%/}
 if [ "$arch" == "i386" -o "$arch" == "amd64" -o "$arch" == "python2" -o "$arch" == "python3" -a "$2" != "" -a "$3" != "" ]; then
    echo "Running for $arch:"
    cd "$arch"
+   echo "  Creating $2_v$3 folder..."
    mkdir "$2_v$3" && cd $2_v$3
-   echo "Standard display file for $2" > $2_display.txt
+   echo "  Creating $2_display.txt..."
+   echo "standard display file for $2" > $2_display.txt
+   echo "  Creating $2_install.sh..."
    touch $2_install.sh && chmod ugo+x $2_install.sh
+   echo "  Creating nofile..."
    touch nofile
    echo "  remove 'nofile' if binary/script is called $2[.bin/.sh/.py]"
    echo "Done for $arch!"
