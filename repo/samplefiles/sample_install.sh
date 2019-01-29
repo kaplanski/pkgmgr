d5a38cabe92f57691a6bb607ff1ffa9e05566aad
Jan-Daniel Kaplanski <kaplanski@uni-potsdam.de>
2019-01-29 17:40:53 +0100

    repo update

--- repo/samplefiles/sample_install.sh
+++ repo/samplefiles/sample_install.sh
@@ -28,6 +28,17 @@ if [ "$ver" == "lite" ]; then
    #echo 'echo "   2. APP"' >> $infldr/$pkgname/$pkgname.sh
    #echo 'echo "   3. APP"' >> $infldr/$pkgname/$pkgname.sh
    #just add your apps to this list
+   echo 'echo "----------------------"' >> $infldr/$pkgname/$pkgname.sh
+   echo 'read -p "Enter a number: " num' >> $infldr/$pkgname/$pkgname.sh
+   echo 'if [ "$num" == "1" ]; then' >> $infldr/$pkgname/$pkgname.sh
+   echo '   exec ./app1' >> $infldr/$pkgname/$pkgname.sh
+   echo 'elif [ "$num" == "2" ]; then' >> $infldr/$pkgname/$pkgname.sh
+   echo '   exec ./app2' >> $infldr/$pkgname/$pkgname.sh
+   echo 'elif [ "$num" == "3" ]; then' >> $infldr/$pkgname/$pkgname.sh
+   echo '   exec ./app3' >> $infldr/$pkgname/$pkgname.sh
+   echo 'else' >> $infldr/$pkgname/$pkgname.sh
+   echo '   echo "Unknown option!"' >> $infldr/$pkgname/$pkgname.sh
+   echo 'fi' >> $infldr/$pkgname/$pkgname.sh
    chmod ugo+x $infldr/$pkgname/$pkgname.sh
 fi
 echo "Done!"
