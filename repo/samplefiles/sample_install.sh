405bc2b38629470623d499b0988b431de104fa29
Jan-Daniel Kaplanski <kaplanski@uni-potsdam.de>
2019-04-28 04:37:29 +0200

    repo update

--- repo/samplefiles/sample_install.sh
+++ repo/samplefiles/sample_install.sh
@@ -1,24 +1,17 @@
 #automated install script
 #leave settings on default to work!
-#ver is either 'lite' or 'full'
+#$ver is either 'lite' or 'full' (old bash pkgmgr versions)
 
-pkgfldr=$1
-infldr=$2
-pkgname=$3
+pkgfldr=$1 # pkgmgr folder. default: $HOME/pkgmgr
+infldr=$2 # bin folder. default: $HOME/pkgmgr/bin
+pkgname=$3 # pkgname (if pkg=abc_v0.1b.tgz; then pkgname=abc)
 
-#if [ "$4" == "" ]; then #depreciated
-#   ver="lite"
-#else
-#   ver=$4
-#fi
-
+#create pkg folder in $HOME/pkgmgr/bin
 if [ ! -d "$infldr/$pkgname" ]; then
-   echo "Initial creation of $pkgfldr/$pkgname!"
+   echo "Initial creation of $infldr/$pkgname!"
    mkdir "$infldr/$pkgname"
    echo "Done!"
 fi
 
-#echo "Installing $pkgname..." #depreciated
+#place your install instructions here (i.e. cp)
 cp * "$infldr/$pkgname"
-
-#echo "Done!" depreciated
