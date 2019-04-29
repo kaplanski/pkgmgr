aafaabdf75b1dab826d1a7e913845eea25056e8d
Jan-Daniel Kaplanski <kaplanski@uni-potsdam.de>
2019-04-29 15:15:26 +0200

    package index build now works as intended

--- repo/build-index-db.sh
+++ repo/build-index-db.sh
@@ -1,26 +1,12 @@
 #!/usr/bin/env bash
 arch=${1%/}
+
 if [ "$arch" == "stable" ]; then
+
    echo "Running for $arch:"
    cd "$arch"
-   for dir in */; do
-      base=$(basename "$dir")
-      if [ "$base" != "*" ]; then
-         echo "  Building $base..."
-         tar -czf "${base}.tgz" "$dir"
-         rm -rf "$dir"
-      else
-         echo "  nothing to build..."
-         echo "Done for $arch!"
-         exit 1
-      fi
-   done
 
-   if [ -f index.db ]; then
-      echo "  removing old index..."
-      rm index.db
-   fi
-
+   #get last ID
    if [ ! -f "index.db" ]; then
       ID_cnt=1
       echo "[ID:Name:Version:Dependencies]" >> index.db
@@ -28,13 +14,24 @@ if [ "$arch" == "stable" ]; then
       ID_cnt=$(( $(tail -n 1 "index.db" | cut -d ":" -f 1 | cut -d "{" -f 2) + 1 ))
    fi
 
-   for f in *_v*.tgz; do
-      if [ "$f" != "*_v*.tgz" ]; then
-         p_ver="${f#*_v}"
-         ver="${p_ver%.tgz}"
-         echo "Enter dependencies for ${f%_v*} (or leave blank; delim: ,) [ENTER]:"; read deps
-         echo "{$ID_cnt:${f%_v*}:$ver:$deps}" >> index.db 2>/dev/null
+   for dir in */; do
+      base=$(basename "$dir")
+      if [ "$base" != "*" ]; then
+         #building package
+         echo "  Building $base..."
+         tar -czf "${base}.tgz" "$dir"
+         rm -rf "$dir"
+
+         #generating index
+         p_ver="${dir#*_v}"
+         ver="${p_ver%/}"
+         echo "Enter dependencies for ${dir%_v*} (or leave blank; delim: ,) [ENTER]:"; read deps
+         echo "{$ID_cnt:${dir%_v*}:$ver:$deps}" >> index.db 2>/dev/null
          ID_cnt=$(($ID_cnt + 1))
+      else
+         echo "  nothing to build..."
+         echo "Done for $arch!"
+         exit 1
       fi
    done
    echo "  new index created!"
